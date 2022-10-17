39fe262f820f6ae24fbe4c149d17578ecd64584a
Elin Forsberg <elinfsp96@hotmail.com>
2022-10-17 14:25:24 +0200

    list of products

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-
-const body = document.getElementById("body");
-const h = document.createElement("div");
-div.classList.add("css-for-div");
-
-body.textContent = "Hello world";
-console.log(h);
-
-
-
-
 var listOfProducts;
 
 /** Get products from the json file and store it in a gobal variable */
@@ -34,27 +23,36 @@ function addProductsToWebpage() {
     // Check your console to see that the products are stored in the listOfProducts varible.
     console.log(listOfProducts);
 
-    const title = listOfProducts.map(listOfProducts => listOfProducts.title)
-    console.log(title);
+    const main = document.querySelector("main");
+    for (const product of listOfProducts) {
 
-    const description = listOfProducts.map(listOfProducts => listOfProducts.description)
-    console.log(description);
+        const div =document.createElement("div");
+        div.classList.add("productcontainer")
+        main.appendChild(div)
 
-    const price = listOfProducts.map(listOfProducts => listOfProducts.price)
-    console.log(price);
+        const h1 = document.createElement("h1");
+        h1.classList.add("css-for-title")
+        h1.innerText = product.title
+        div.appendChild(h1)
 
+        const h3 = document.createElement("h3");
+        h3.classList.add("css-for-description")
+        h3.innerText = product.description
+        div.appendChild(h3)
 
-    
-       
+       const h2 = document.createElement("h2");
+       h2.classList.add("css-for-price") 
+       h2.innerText = product.price
+       div.appendChild(h2)
+    }
+
+}
+
+   
+     
+
+   
         
-        
-    const div = document.createElement("div");
-
-
-    div.innerText = price;
-    document.body.appendChild(div);
-
-
     
     
     
@@ -64,4 +62,4 @@ function addProductsToWebpage() {
     // an explainetory comment like the one for this function, see row 22.
     
     // TODO: Remove the console.log and these comments when you've read them.
-}
+//}
