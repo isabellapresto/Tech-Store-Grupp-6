46af435351e415297cfbe5239f9d92f184772555
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2022-10-25 15:37:04 +0200

    funktioner för att logga in samt ny medlem

---
+++ login.js
@@ -0,0 +1,72 @@
+let userArray = [
+    {
+        username: "admin",
+        password: "1234"
+    },
+    {
+        username: "admin2",
+        password: "5678"
+    },
+]
+
+if (!localStorage.getItem("userArray")) {
+    localStorage.setItem("userArray", JSON.stringify(userArray));
+} 
+
+const username = document.querySelector(".username");
+const password = document.querySelector(".password");
+const btnLogIn = document.getElementById("btn-login");
+const newUserInput = document.querySelector(".newuser");
+const newPasswordInput = document.querySelector(".newpsw");
+const btnNewMember = document.getElementById("btn-newmember");
+
+btnLogIn.addEventListener("click", checkLogIn);
+btnNewMember.addEventListener("click", addNewUser);
+console.log(btnLogIn);
+
+function initSite() {
+
+if (localStorage.getItem("username")) {
+    logInSuccess();
+}
+}
+
+function checkLogIn() {
+
+    const getMyUsers = localStorage.getItem("userArray");
+    const userArrayUpdate = JSON.parse(getMyUsers);
+
+    for(i=0; i < userArrayUpdate.length; i++) {
+        if(username.value === userArrayUpdate[i].username && password.value === userArrayUpdate[i].password) {
+            localStorage.setItem("username", username.value);     
+            logInSuccess();
+            return
+        }
+    }
+    logInFail();
+}
+
+function logInSuccess() {
+    console.log("Du är inloggad");
+}
+
+function logInFail() {
+    console.log("Oops försök igen");
+}
+
+
+function addNewUser(){
+
+    const newUser = {
+        username: newUserInput.value,
+        password: newPasswordInput.value
+    }
+
+    let getAllUser = JSON.parse(localStorage.getItem("userArray"));
+
+    getAllUser.push(newUser);
+
+    localStorage.setItem("userArray", JSON.stringify(getAllUser));
+
+    console.log("Du är nu medlem, hurra!")
+}
